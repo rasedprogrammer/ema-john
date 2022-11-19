1ffb313f26b2ebb024992034f6a0039c592fabad
rasedprogrammer <rasedprogrammer@gmail.com>
2022-11-19 20:29:25 +0600

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Main from "./layouts/Main";
 import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
 
 function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Main></Main>,
+			children: [
+				{ path: "/", element: <Shop></Shop> },
+				{ path: "/orders", element: <Orders></Orders> },
+				{ path: "/inventory", element: <Inventory></Inventory> },
+			],
+		},
+	]);
 	return (
 		<div>
-			<Header></Header>
-			<Shop></Shop>
+			<RouterProvider router={router}></RouterProvider>
 		</div>
 	);
 }
